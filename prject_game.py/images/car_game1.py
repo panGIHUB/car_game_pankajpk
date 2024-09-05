0f39a5b510585c0381838eff582c6d31b2cb9f31
panGIHUB <135425601+panGIHUB@users.noreply.github.com>
2024-09-05 20:47:01 +0530

    Add files via upload

---
+++ prject_game.py/images/car_game1.py
@@ -0,0 +1,215 @@
+import pygame
+from pygame.locals import *
+import random
+
+pygame.init()
+
+# create the window
+width = 500
+height = 500
+screen_size = (width, height)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Car Game')
+
+# colors
+gray = (100, 100, 100)
+green = (76, 208, 56)
+red = (200, 0, 0)
+white = (255, 255, 255)
+yellow = (255, 232, 0)
+
+# road and marker sizes
+road_width = 300
+marker_width = 10
+marker_height = 50
+
+# lane coordinates
+left_lane = 150
+center_lane = 250
+right_lane = 350
+lanes = [left_lane, center_lane, right_lane]
+
+# road and edge markers
+road = (100, 0, road_width, height)
+left_edge_marker = (95, 0, marker_width, height)
+right_edge_marker = (395, 0, marker_width, height)
+
+# for animating movement of the lane markers
+lane_marker_move_y = 0
+
+# player's starting coordinates
+player_x = 250
+player_y = 400
+
+# frame settings
+clock = pygame.time.Clock()
+fps = 120
+
+# game settings
+gameover = False
+speed = 2
+score = 0
+high_score = 0
+
+# load sounds
+collision_sound = pygame.mixer.Sound('sounds/collision.wav.mp3')
+power_up_sound = pygame.mixer.Sound('sounds/power_up.wav.mp3')
+
+class Vehicle(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        image_scale = 45 / image.get_rect().width
+        new_width = image.get_rect().width * image_scale
+        new_height = image.get_rect().height * image_scale
+        self.image = pygame.transform.scale(image, (new_width, new_height))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+class PlayerVehicle(Vehicle):
+    def __init__(self, x, y):
+        image = pygame.image.load('images/car.png')
+        super().__init__(image, x, y)
+
+class PowerUp(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(image, (30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+# sprite groups
+player_group = pygame.sprite.Group()
+vehicle_group = pygame.sprite.Group()
+powerup_group = pygame.sprite.Group()
+
+# create the player's car
+player = PlayerVehicle(player_x, player_y)
+player_group.add(player)
+
+# load the vehicle images
+image_filenames = ['pickup_truck.png', 'semi_trailer.png', 'taxi.png', 'van.png']
+vehicle_images = [pygame.image.load('images/' + fn) for fn in image_filenames]
+
+# load the power-up image
+powerup_image = pygame.image.load('images/power_up.jpg')
+
+# load the crash image
+crash = pygame.image.load('images/crash.png')
+crash_rect = crash.get_rect()
+
+# game loop
+running = True
+while running:
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT and player.rect.center[0] > left_lane:
+                player.rect.x -= 100
+            elif event.key == K_RIGHT and player.rect.center[0] < right_lane:
+                player.rect.x += 100
+            for vehicle in vehicle_group:
+                if pygame.sprite.collide_rect(player, vehicle):
+                    gameover = True
+                    pygame.mixer.Sound.play(collision_sound)
+                    if event.key == K_LEFT:
+                        player.rect.left = vehicle.rect.right
+                        crash_rect.center = [player.rect.left, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
+                    elif event.key == K_RIGHT:
+                        player.rect.right = vehicle.rect.left
+                        crash_rect.center = [player.rect.right, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
+
+    screen.fill(green)
+    pygame.draw.rect(screen, gray, road)
+    pygame.draw.rect(screen, yellow, left_edge_marker)
+    pygame.draw.rect(screen, yellow, right_edge_marker)
+
+    lane_marker_move_y += speed * 2
+    if lane_marker_move_y >= marker_height * 2:
+        lane_marker_move_y = 0
+    for y in range(marker_height * -2, height, marker_height * 2):
+        pygame.draw.rect(screen, white, (left_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
+        pygame.draw.rect(screen, white, (center_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
+
+    player_group.draw(screen)
+
+    if len(vehicle_group) < 2:
+        add_vehicle = True
+        for vehicle in vehicle_group:
+            if vehicle.rect.top < vehicle.rect.height * 1.5:
+                add_vehicle = False
+        if add_vehicle:
+            lane = random.choice(lanes)
+            image = random.choice(vehicle_images)
+            vehicle = Vehicle(image, lane, height / -2)
+            vehicle_group.add(vehicle)
+
+    for vehicle in vehicle_group:
+        vehicle.rect.y += speed
+        if vehicle.rect.top >= height:
+            vehicle.kill()
+            score += 1
+            if score > 0 and score % 5 == 0:
+                speed += 1
+
+    # Add power-ups randomly
+    if random.randint(1, 100) > 98 and len(powerup_group) < 2:
+        lane = random.choice(lanes)
+        powerup = PowerUp(powerup_image, lane, height / -2)
+        powerup_group.add(powerup)
+
+    for powerup in powerup_group:
+        powerup.rect.y += speed
+        if powerup.rect.top >= height:
+            powerup.kill()
+        if pygame.sprite.collide_rect(player, powerup):
+            pygame.mixer.Sound.play(power_up_sound)
+            score += 10
+            powerup.kill()
+
+    vehicle_group.draw(screen)
+    powerup_group.draw(screen)
+
+    font = pygame.font.Font(pygame.font.get_default_font(), 16)
+    text = font.render('Score: ' + str(score), True, white)
+    text_rect = text.get_rect()
+    text_rect.center = (50, 400)
+    screen.blit(text, text_rect)
+
+    if pygame.sprite.spritecollide(player, vehicle_group, True):
+        gameover = True
+        pygame.mixer.Sound.play(collision_sound)
+        crash_rect.center = [player.rect.center[0], player.rect.top]
+
+    if gameover:
+        screen.blit(crash, crash_rect)
+        pygame.draw.rect(screen, red, (0, 50, width, 100))
+        font = pygame.font.Font(pygame.font.get_default_font(), 16)
+        text = font.render('Game over. Play again? (Enter Y or N)', True, white)
+        text_rect = text.get_rect()
+        text_rect.center = (width / 2, 100)
+        screen.blit(text, text_rect)
+
+    pygame.display.update()
+
+    while gameover:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                gameover = False
+                running = False
+            if event.type == KEYDOWN:
+                if event.key == K_y:
+                    gameover = False
+                    speed = 2
+                    score = 0
+                    vehicle_group.empty()
+                    powerup_group.empty()
+                    player.rect.center = [player_x, player_y]
+                elif event.key == K_n:
+                    gameover = False
+                    running = False
+
+pygame.quit()
